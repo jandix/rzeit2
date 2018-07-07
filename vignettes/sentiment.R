89752eda88efe4dd7840cbece1ae95a159ab891e
Jan Dix <dix.jan@gmx.de>
2018-07-07 14:43:00 +0200

    update vignette

--- vignettes/sentiment.R
+++ vignettes/sentiment.R
@@ -30,10 +30,11 @@ robots$permissions
 #                         text = article_texts,
 #                         date = as.Date(articles_merkel$content$release_date),
 #                         stringsAsFactors = F)
+#  
+#  # exclude premium content
 #  articles <- articles %>%
 #    filter(!str_detect(text, "ZEIT PLUS CONTENT"))
-
-## ----calculate sentiment, eval = FALSE-----------------------------------
+#  
 #  # lazy loading german sentiment dictionary
 #  data("senti_ws")
 #  
@@ -56,7 +57,10 @@ sentiment <- sentiment_example %>%
 # plot the sentiment by day
 ggplot(sentiment, aes(date, sentiment)) +
   geom_point(pch = 1, col = "#3a9b96") +
-  geom_line(col = "#3a9b96", alpha = .9) +
+  geom_line(col = "#3a9b96", alpha = .6) +
+  geom_hline(yintercept = mean(sentiment$sentiment), col = "#4e4e4e") +
+  annotate("text", x = as.Date("2018-05-30"), y = -.18, 
+           label = sprintf("mean = %s", round(mean(sentiment$sentiment), 2)), col = "#4e4e4e") +
   theme_fivethirtyeight() +
   theme(axis.title = element_text(), axis.title.x = element_blank()) + ylab('Articles Sentiment')
 
