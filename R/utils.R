fd50f10150312d6a0fddfe02e9fd58db464f4db1
Jan Dix <dix.jan@gmx.de>
2018-08-09 15:31:40 +0200

    update utility functions

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,67 @@
 ##----------------------------------------------------------------------------##
+##                                  fetch_article                             ##
+##----------------------------------------------------------------------------##
+
+#' Get ZEIT ONLINE article
+#'
+#' @description Download and process article.
+#'
+#' @param url character. A ZEIT ONLINE url.
+#' @return xml_document. A list of class xml_document returned by \code{xml2::read_html}.
+#' @importFrom rvest html_nodes
+#' @importFrom httr GET
+#' @importFrom httr http_error
+#' @importFrom httr status_code
+#' @importFrom httr content
+#' @importFrom xml2 read_html
+#' @keywords internal
+#' @export
+#' @noRd
+
+get_article <- function (url) {
+
+  # define empty article
+  article <- NULL
+
+  # try to download article
+  response <- httr::GET(url)
+
+  # check if http error
+  if (httr::http_error(response)) {
+    stop(
+      sprintf(
+        "Article parse failed [%s]",
+        httr::status_code(response)
+      ),
+      call. = FALSE
+    )
+  }
+
+  # parse article
+  article <- xml2::read_html(httr::content(response, "text"))
+
+  # check if is zeit plus
+  if (is_zeit_plus(article)) {
+    return(list(
+      article = article,
+      zeit_plus = TRUE
+    ))
+  }
+
+  if(has_multiple_pages(article)) {
+    # adjust original url
+    url <- paste0(url, "/komplettansicht")
+    # refetch article url
+    return(rzeit2::get_article(url))
+  }
+
+  list(
+    article = article,
+    zeit_plus = FALSE
+  )
+}
+
+##----------------------------------------------------------------------------##
 ##                                  is_zeit_plus                              ##
 ##----------------------------------------------------------------------------##
 
@@ -6,7 +69,7 @@
 #'
 #' @description Check if a given article is a ZEIT PLUS article (and not accessible without payment).
 #'
-#' @param article
+#' @param article xml_document. A list of class xml_document returned by \code{xml2::read_html}.
 #' @return logical
 #' @importFrom rvest html_nodes
 #' @keywords internal
@@ -29,13 +92,13 @@ is_zeit_plus <- function (article) {
 #'
 #' @description Check if a given article has multiple pages.
 #'
-#' @param article
+#' @param article xml_document. A list of class xml_document returned by \code{xml2::read_html}.
 #' @return logical
 #' @importFrom rvest html_nodes
 #' @keywords internal
 #' @noRd
 
-has_multiple_pages <- function () {
+has_multiple_pages <- function (article) {
 
   # check if multiple pages
   nodes <- rvest::html_nodes(article, "a.article-toc__onesie")
