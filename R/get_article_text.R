c367c66a6c200f94eaaa2a3b597aed42aa020e21
Jan Dix <dix.jan@gmx.de>
2018-08-11 14:43:02 +0200

    apply helper functions

--- R/get_article_text.R
+++ R/get_article_text.R
@@ -28,59 +28,24 @@ get_article_text <- function (url,
     stop("Please provide valid ZEIT ONLINE URL(s).", call. = FALSE)
   }
 
-  fetch_article <- function (url, timeout) {
-    # define empty article
-    article <- NULL
+  # define helper function
+  get_text <- function (url, timeout) {
 
-    # try to download article
-    response <- httr::GET(url)
+    # get article document
+    article <- rzeit2::get_article(url)
 
-    # check if http error
-    if (httr::http_error(response)) {
-      stop(
-        sprintf(
-          "Article parse failed [%s]",
-          httr::status_code(response)
-        ),
-        call. = FALSE
-      )
-    }
-
-    # parse article
-    article <- xml2::read_html(httr::content(response, "text"))
-
-    if (has_multiple_pages(article)) {
-      # adjust original url
-      url <- paste0(url, "/komplettansicht")
-      return(fetch_article(url, timeout = timeout))
-    }
-
-    # set timeout to avoid blocking
-    if(!is.null(timeout)) {
-      Sys.sleep(timeout)
-    }
-
-    # if found article then extract text
-    if(!is.null(article)) {
-
-      if (is_zeit_plus(article)) {
-        # return ZEIT PLUS placeholder
-        text <- "[ZEIT PLUS CONTENT] You need a ZEIT PLUS account to access this content."
-      }
-      else {
-        # extract article text
-        nodes <- rvest::html_nodes(article, ".article-page p")
-        html <- rvest::html_text(nodes)
-        text <- paste(html, collapse = " ")
-      }
-
+    if (!article$zeit_plus) {
+      # extract article text
+      nodes <- rvest::html_nodes(article$article, ".article-page p")
+      html <- rvest::html_text(nodes)
+      text <- paste(html, collapse = " ")
       # define return object
       return(text)
     } else {
-      return(NA)
+      # return placeholder
+      return("[ZEIT PLUS CONTENT] You need a ZEIT PLUS account to access this content.")
     }
   }
-
   # apply function to all urls
   sapply(url, try(fetch_article), timeout)
 }
